8b3b174f9396f0fb74541221b34f344fb2defdd4
EdwinDiazF21 <50114639+EdwinDiazF21@users.noreply.github.com>
2021-06-09 11:48:54 -0500

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,110 +1,35 @@
-package com.example.applab3_ediaz_rpedroza_awu;
+package com.example.glovppettito_22_eats;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-
-import com.example.applab3_ediaz_rpedroza_awu.Helpers.CustomDialog;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    //declaramos las variables de los controles que vamos a utilizar
-    EditText txtNum1;
-    EditText txtNum2;
-    TextView lblResultado;
-    Spinner spnOp;
-    CheckBox chkMsg;
-    String opera = "";
-
+    EditText txtuser;
+    EditText txtpassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        this.InicializarControles();
+        Inicializar_Controles();
     }
 
-    private void InicializarControles() {
-        txtNum1 = (EditText) findViewById(R.id.txtNum1);
-        txtNum2 = (EditText) findViewById(R.id.txtNum2);
-        spnOp = (Spinner) findViewById(R.id.spnOp);
-        chkMsg = (CheckBox)findViewById(R.id.chkMsg);
-        lblResultado = (TextView) findViewById(R.id.lblRes);
-
-        this.InicializarSpinner();
+    private void Inicializar_Controles()
+    {
+       txtuser = (EditText)findViewById(R.id.etxt_user);
+        txtpassword = (EditText)findViewById(R.id.etxt_contra);
     }
 
-    public List<String> ObtenerOperacion() {
-        List<String> operacion = new ArrayList<String>();
-        operacion.add("Suma");
-        operacion.add("Resta");
-        operacion.add("Multiplicacion");
-        operacion.add("Division");
+    
 
-        return operacion;
-    }
 
-    public String ObtenerOperacionSeleccionada() {
-        try {
-            return (String) spnOp.getSelectedItem();
-        } catch (Exception e) {
-
-        }
-        return "";
-    }
-
-    public void InicializarSpinner() {
-        ArrayAdapter<String> Operadores = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, this.ObtenerOperacion());
-        spnOp.setAdapter(Operadores);
-    }
-
-    public void Operador(View view) {
-        Integer resultado = 0;
-        String oper = "";
-        try {
-            String opera = this.ObtenerOperacionSeleccionada();
-            int n1 = Integer.parseInt(txtNum1.getText().toString());
-            int n2 = Integer.parseInt(txtNum2.getText().toString());
-
-            if (opera.equals("Suma")) {
-                resultado = n1 + n2;
-                oper = "+";
-            }
-            else if (opera.equals("Resta")) {
-                resultado = n1 - n2;
-                oper = "-";
-            }
-            else if (opera.equals("Multiplicacion")) {
-                resultado = n1 * n2;
-                oper = "x";
-            }
-            else {
-                resultado = n1 / n2;
-                oper= "/";
-            }
-
-            if (chkMsg.isChecked()){
-                FragmentManager fm = getSupportFragmentManager();
-                CustomDialog dialog = new CustomDialog ("RESULTADO",n1 +" " +oper +" " + n2 +" = " +resultado);
-                dialog.show(fm, "DIALOGO");
-            }
-
-            lblResultado.setText("Resultado: " + Integer.toString(resultado));
-
-        } catch (Exception e) {
-            Toast.makeText(this, "Error en la suma de los datos" + e.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-
-    }
 }
